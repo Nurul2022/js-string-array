42b03cc3f1bdbb0614c5dda1f85b299cfd114b1a
Nurul Islam <nurulbdpersonal@gmail.com>
2022-02-13 23:12:49 +0600

    string-slice.js added

--- string-slice.js
+++ string-slice.js
@@ -1,26 +1,26 @@
 const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
 const words = anthem.split(' ');
 const withoutA = anthem.split('a');
-// console.log(withoutA);
+ //console.log(withoutA);
 
 // slice 
 const smallSlice = anthem.slice(5, 13);
-// console.log(smallSlice);
+ //console.log(smallSlice);
 
 // substr
 const anotherPart = anthem.substr(11, 8);
-// console.log(anotherPart);
+ //console.log(anotherPart);
 
 // substring
 const anotherAnotherPart = anthem.substring(11, 15);
-console.log(anotherAnotherPart);
+//console.log(anotherAnotherPart);
 
 //concat
 const first = 'Amader';
 const second = 'City';
 // const fullString = first + second;
 const fullString = first.concat(second).concat('abc').concat('RJ Kibria')
-// console.log(fullString);
+ //console.log(fullString);
 
 // 
 
